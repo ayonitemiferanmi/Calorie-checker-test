ab30b2ca95418b71fc899487b4f03889b3682656
ayonitemiferanmi <52452486+ayonitemiferanmi@users.noreply.github.com>
2024-10-25 11:09:39 +0100

    Update fast_api.py

--- fast_api.py
+++ fast_api.py
@@ -15,13 +15,8 @@ import requests
 
 app = FastAPI()
 
-# temp_file = requests.get("https://huggingface.co/Ayonitemi-Feranmi/calorie_tester/resolve/main/best.pt")
-# model_path_1 = ""
-# with open(model_path_1, "wb") as f:
-#     f.write(temp_file.content)
-
 # Load the YOLO model (you can also include logic to download it from Hugging Face if not available)
-model_path = "best.pt"
+model_path = "latest_best.pt"
 model = YOLOv10(model_path, task='detect')
 
 @app.get("/")
